34f99aefff97504467d97771c59f4025f46b8e00
Dinh Ngoc Hien <hiendinhngoc@yahoo.com.vn>
2023-09-27 00:23:00 +0700

    #25 Root module structure, variables input files (#26)

--- main.tf
+++ main.tf
@@ -1,33 +1,3 @@
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "CoderPush"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "aws" {
-  
-}
-
-provider "random" {
-  # Configuration options
-}
-
 resource "random_string" "bucket_name" {
   lower            = true
   upper            = false
@@ -39,8 +9,8 @@ resource "random_string" "bucket_name" {
 resource "aws_s3_bucket" "example" {
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result
-}
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "user_uuid" {
+  type = string
+  validation {
+    condition = can(regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.user_uuid))
+    error_message = "User UUID must be in the format xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  }
+}
