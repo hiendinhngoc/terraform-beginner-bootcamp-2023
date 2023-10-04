3c029369a0dcdc6efcaaa099fb79531e989e709a
Dinh Ngoc Hien <hiendinhngoc@yahoo.com.vn>
2023-10-05 00:13:34 +0700

    #29 Refactor s3 bucket in to new terrahouse_aws module (#30)

--- variables.tf
+++ variables.tf
@@ -1,19 +1,7 @@
 variable "user_uuid" {
   type = string
-  validation {
-    condition = can(regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.user_uuid))
-    error_message = "User UUID must be in the format xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-  }
 }
 
 variable "bucket_name" {
   type = string
-
-  validation {
-    condition = (
-      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 &&
-      can(regex("^([a-z0-9]{1}[a-z0-9-]{1,61}[a-z0-9]{1})$", var.bucket_name))
-    )
-    error_message = "Bucket name must be between 3 and 63 characters long. Invalid bucket name, please check https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html for more details."
-  }
 }
