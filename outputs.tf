a17248b261b9bf59f15c9acdbb484de467ec34c1
Dinh Ngoc Hien <hiendinhngoc@yahoo.com.vn>
2023-10-05 17:14:38 +0000

    #31 enable static website hosting, upload static files to s3

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   description = "Bucket name for the static website hosting"
   value = module.terrahouse_aws.bucket_name
 }
+
+output "website_endpoint" {
+  description = "s3 static website hosting"
+  value = module.terrahouse_aws.website_endpoint
+}
