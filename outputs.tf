93b1bffc5dfc6df48f90efef481242690b203538
Dinh Ngoc Hien <hiendinhngoc@yahoo.com.vn>
2023-10-07 23:33:21 +0700

    #37 Invalidate cloudfront (#38)

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "website_endpoint" {
   description = "s3 static website hosting"
   value = module.terrahouse_aws.website_endpoint
 }
+
+output "cloudfront_url" {
+  description = "The Cloudfront distribution domain name"
+  value = module.terrahouse_aws.cloudfront_url
+}
