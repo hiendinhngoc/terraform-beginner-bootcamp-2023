11f4799f386922e219633565f1fa0b506f165b29
Dinh Ngoc Hien <hiendinhngoc@yahoo.com.vn>
2023-10-13 10:24:59 +0000

    #41 refactor server code

--- terratowns_mock_server/server.rb
+++ terratowns_mock_server/server.rb
@@ -23,7 +23,7 @@ class Home
   # eg:
   # home = new Home()
   # home.town = 'hello' # setter
-  # hone.town() #getter
+  # home.town() #getter
   attr_accessor :town, :name, :description, :domain_name, :content_version
 
   validates :town, presence: true, inclusion: {
@@ -204,7 +204,6 @@ class TerraTownsMockServer < Sinatra::Base
     # Validate payload data
     name = payload["name"]
     description = payload["description"]
-    domain_name = payload["domain_name"]
     content_version = payload["content_version"]
 
     unless params[:uuid] == $home[:uuid]
@@ -213,9 +212,9 @@ class TerraTownsMockServer < Sinatra::Base
 
     home = Home.new
     home.town = $home[:town]
+    home.domain_name = $home[:domain_name]
     home.name = name
     home.description = description
-    home.domain_name = domain_name
     home.content_version = content_version
 
     unless home.valid?
@@ -226,7 +225,6 @@ class TerraTownsMockServer < Sinatra::Base
   end
 
   # DELETE
-  # delete the mock from the database
   delete '/api/u/:user_uuid/homes/:uuid' do
     ensure_correct_headings
     find_user_by_bearer_token
@@ -237,8 +235,10 @@ class TerraTownsMockServer < Sinatra::Base
       error 404, "failed to find home with provided uuid and bearer token"
     end
 
+    # delete the mock from the database
+    uid = $home[:uuid]
     $home = {}
-    { message: "House deleted successfully" }.to_json
+    { uuid: uuid }.to_json
   end
 end
 
