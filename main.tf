05fc3c868c1e8a138559f5bb246b207f4a0b7eb0
Dinh Ngoc Hien <hiendinhngoc@yahoo.com.vn>
2023-10-14 03:53:49 +0000

    Update town

--- main.tf
+++ main.tf
@@ -36,33 +36,30 @@ module "home_arcanum_hosting" {
 }
 
 resource "terratowns_home" "home" {
-  name = "Empower Pioneers"
+  name = "My favorite artist"
   description = <<DESCRIPTION
-CoderPush is a remote-first software consultant for businesses.
-From small companies to big enterprises, we support them to scale their products and services.
-With no fixed address, we free ourselves from the physical location to extend our abilities in supporting businesses.
-We have scaled our own dream of reaching the world to be true. Now it's your chance. Let us give you a helping hand.
+My favorite artist is Den Vau, he is a famous rapper in Viet Nam, his real name is Nguyen Duc Cuong. He is one of the few successful artists from underground.
   DESCRIPTION
   domain_name = module.home_arcanum_hosting.domain_name
   # domain_name = "coderpush.cloudfront.net"
-  town = "missingo"
+  town = "melomaniac-mansion"
   content_version = var.arcanum.content_version
 }
 
-module "home_payday_hosting" {
-  source = "./modules/terrahome_aws"
-  public_path = var.payday.public_path
-  user_uuid = var.user_uuid
-  content_version = var.payday.content_version
-}
+# module "home_payday_hosting" {
+#   source = "./modules/terrahome_aws"
+#   public_path = var.payday.public_path
+#   user_uuid = var.user_uuid
+#   content_version = var.payday.content_version
+# }
 
-resource "terratowns_home" "home_payday" {
-  name = "Pushing Boundary"
-  description = <<DESCRIPTION
-scale your team. nail your dream.
-  DESCRIPTION
-  domain_name = module.home_payday_hosting.domain_name
-  # domain_name = "coderpush.cloudfront.net"
-  town = "missingo"
-  content_version = var.payday.content_version
-}
+# resource "terratowns_home" "home_payday" {
+#   name = "Pushing Boundary"
+#   description = <<DESCRIPTION
+# scale your team. nail your dream.
+#   DESCRIPTION
+#   domain_name = module.home_payday_hosting.domain_name
+#   # domain_name = "coderpush.cloudfront.net"
+#   town = "melomaniac-mansion"
+#   content_version = var.payday.content_version
+# }
